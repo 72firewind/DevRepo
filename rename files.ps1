2d62a1ce1e57d81055c849dd88518c19f149bbe1
72firewind <rlamothe@vmti.ca>
2021-01-13 12:19:50 -0500

    relocated files

--- rename files.ps1
+++
@@ -1,5 +0,0 @@
-﻿
-
-Get-ChildItem -Filter "*The.Big.Bang.Theory*" -Recurse | Rename-Item -NewName {$_.name -replace 'The.Big.Bang.Theory','TBBT' }
-
-Get-ChildItem -Filter "*The Big Bang Theory*" -Recurse | Rename-Item -NewName {$_.name -replace 'The Big Bang Theory','TBBT' }
