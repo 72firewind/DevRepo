7868f8a4a60803b16a1ea75f99e077217fd99dd0
72firewind <rlamothe@vmti.ca>
2021-01-13 12:48:13 -0500

    added line

---
+++ rename files.ps1
@@ -0,0 +1,6 @@
+﻿
+
+Get-ChildItem -Filter "*The.Big.Bang.Theory*" -Recurse | Rename-Item -NewName {$_.name -replace 'The.Big.Bang.Theory','TBBT' }
+
+Get-ChildItem -Filter "*The Big Bang Theory*" -Recurse | Rename-Item -NewName {$_.name -replace 'The Big Bang Theory','TBBT' }
+#Test-Certificate  
