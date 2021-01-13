ba02e75c8efddfec69e6c0caf31c3c848244a37e
72firewind <rlamothe@vmti.ca>
2021-01-13 12:13:22 -0500

    first commit

---
+++ rename files.ps1
@@ -0,0 +1,5 @@
+﻿
+
+Get-ChildItem -Filter "*The.Big.Bang.Theory*" -Recurse | Rename-Item -NewName {$_.name -replace 'The.Big.Bang.Theory','TBBT' }
+
+Get-ChildItem -Filter "*The Big Bang Theory*" -Recurse | Rename-Item -NewName {$_.name -replace 'The Big Bang Theory','TBBT' }
